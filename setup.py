8575f296cb65507ac7c62abd234b6e06de7715bb
Kyle Brown <kyle.brown@collectivemedicaltech.com>
2025-01-16 21:52:34 -0700

    Update sci-learn

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dimitrismistriotis/alt-profanity-check",
     packages=setuptools.find_packages(),
-    install_requires=["scikit-learn==1.5.2", "joblib>=1.4.2"],
+    install_requires=["scikit-learn==1.6.0", "joblib>=1.4.2"],
     python_requires=">=3.9",
     package_data={"profanity_check": ["data/model.joblib", "data/vectorizer.joblib"]},
     classifiers=[
